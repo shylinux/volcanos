d6dd93e4064d176a66d3e8df2038432af3446b83
shaoying <shaoying@zuoyebang.com>
2020-01-02 19:10:33 +0800

    add share

--- pane/Storm.js
+++ pane/Storm.js
@@ -4,6 +4,9 @@ Volcanos("onimport", {help: "导入数据", list: [],
             can.Export(event, line.key, "storm")
         })
     },
+    layout: function(event, can, value, cmd, output) {
+        can.Conf("layout", value)
+    },
     river: function(event, can, value, cmd, output) {
         if (value == "update") {return}
         can.run(event, [can.Conf("river", value)], function(msg) {
@@ -15,6 +18,8 @@ Volcanos("onimport", {help: "导入数据", list: [],
             can.run(event, [can.Conf("river")], function(msg) {
                 can.onimport.init(event, can, msg, cmd, output)
             })
+        } else {
+            can.Conf("storm", value)
         }
     },
     favor: function(event, can, msg, cmd, output) {var key = msg.detail[0];
@@ -29,7 +34,7 @@ Volcanos("onimport", {help: "导入数据", list: [],
         })
     },
 })
-Volcanos("onaction", {help: "组件交互", list: ["创建", "刷新"],
+Volcanos("onaction", {help: "组件交互", list: ["创建", "刷新", "共享"],
     "创建": function(event, can, meta, cmd, output) {
         can.Export(event, "create", "steam")
     },
@@ -37,6 +42,9 @@ Volcanos("onaction", {help: "组件交互", list: ["创建", "刷新"],
         can.run(event, [can.Conf("river")], function(msg) {
             can.onimport.init(event, can, msg, cmd, output)
         })
+    },
+    "共享": function(event, can, meta, cmd, output) {
+        can.ondetail[cmd](event, can, meta, "", cmd, output)
     },
 })
 Volcanos("onchoice", {help: "组件菜单", list: ["创建", "刷新"],
@@ -49,7 +57,18 @@ Volcanos("onchoice", {help: "组件菜单", list: ["创建", "刷新"],
         })
     },
 })
-Volcanos("ondetail", {help: "组件详情", list: ["保存", "恢复", "重命名", "删除"],
+Volcanos("ondetail", {help: "组件详情", list: ["共享", "保存", "恢复", "重命名", "删除"],
+    "共享": function(event, can, line, value, cmd, item) {can.share || (can.share = {});
+        var p = can.user.Share(can, {layout: can.Conf("layout"), river: can.Conf("river"), storm: can.Conf("storm")}, true)
+        if (can.share[p]) {
+            can.user.toast({duration: 10000, height: 300, text: p, list: [{img: [can.user.Share(can, {path: "/share/"+can.share[p]}, true)]}]});
+        } else {
+            can.run(event, [can.Conf("river"), can.Conf("storm"), "share", "qrcode", can.Conf("storm"), p], function(msg) {
+                can.share[p] = msg.Result();
+                can.user.toast({duration: 10000, height: 300, text: p, list: [{img: [can.user.Share(can, {path: "/share/"+msg.Result()}, true)]}]});
+            })
+        }
+    },
     "重命名": function(event, can, line, value, cmd, item) {
         can.user.prompt("输入新名：", function(name) {
             can.run(event, [can.Conf("river"), value, "rename", name], function(msg) {
