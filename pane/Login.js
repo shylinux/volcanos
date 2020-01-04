acbe8f21cb13f0efd058dd320ec16f7409e0ce5a
shylinux <shylinux@icloud.com>
2020-01-05 01:22:19 +0800

    add wss

--- pane/Login.js
+++ pane/Login.js
@@ -1,9 +1,9 @@
 Volcanos("onimport", {help: "导入数据", list: [],
     _init: function(can, conf, output, action, option, field) {
         var ui = can.page.Appends(can, option, [
-            {text: ["username: ", "label"]}, {username: []}, {type: "br"},
-            {text: ["password: ", "label"]}, {password: []}, {type: "br"},
-            {button: ["login", function(event, cmd) {
+            {text: ["账号: ", "label"]}, {username: []}, {type: "br"},
+            {text: ["密码: ", "label"]}, {password: []}, {type: "br"},
+            {button: ["密码登录", function(event, cmd) {
                 if (!ui.username.value) {ui.username.focus(); return}
                 if (!ui.password.value) {ui.password.focus(); return}
 
@@ -17,33 +17,47 @@ Volcanos("onimport", {help: "导入数据", list: [],
                 event.stopPropagation()
                 event.preventDefault()
                 return true
-            }]}, {type: "br"},
+            }]},
+            {button: ["扫码登录", function(event, cmd) {
+                can.onimport.username(event, can, "", function(event, msg) {
+                    switch (msg.detail[0]) {
+                        case "space":
+                            if (msg.detail[1] == "share") {can._share = msg.detail[2]
+                                can.user.toast({title: "请用微信扫描", list: [{img: [can.user.Share(can, {
+                                    path: "/share/"+msg.detail[2],
+                                }, true)]}]})
+                                return true
+                            }
+                            break
+                        case "sessid":
+                            can.user.Cookie(can, "sessid", msg.detail[1])
+                            can.Hide(), can.Export(event, "", "login")
+                            can.user.toast("")
+                            return true
+                    }
+                })
+                event.stopPropagation()
+                event.preventDefault()
+                return true
+            }]},
+            {type: "br"},
         ])
     },
-    username: function(event, can, value, cmd, output) {
-        return can._socket || (can._socket = can.misc.WSS(can, function(event, m) {
-            if (m.detail) {
-                switch (m.detail[0]) {
-                    case "share":
-                    case "pwd":
-                        m.Echo("hello world")
+    username: function(event, can, value, cmd, output) {can._username = value
+        return can._socket = can._socket || can.misc.WSS(can, function(event, msg) {
+            if (msg.Option("_handle")) {return can.user.toast(msg.result.join(""))}
+            if (typeof cmd == "function" && cmd(event, msg)) {return msg.Reply(msg)}
 
-                }
-                m.Reply(m)
-            } else {
-                page.toast.Pane.Show(m.result.join(""))
+            switch (msg.detail[0]) {
+                case "space": can._share = msg.detail[2]; break
+                case "pwd": msg.Echo("hello world"); break
             }
-
-        }, function() {
-            can._socket.close()
-
-        }, null, function() {
-            can.user.toast("WSS Open")
-        }))
+            msg.Reply(msg)
+        })
     },
     login: function(event, can, value, cmd, output) {
         if (!can.user.Cookie("sessid")) {can.Show(event, 400, 400); return}
-            can.run(event||{}, ["check"], function(msg) {var user = msg.Result()
+            can.run(event||{}, ["check"], function(msg) {var user = msg.nickname && msg.nickname[0] || msg.Result()
                 user? can.Export(event, user, "username"): can.Show(event, -1, -1)
             })
     },
