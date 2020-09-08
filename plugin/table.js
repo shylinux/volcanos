6d77f4bb21eae42633ef92d7f43e7961c9cee32b
shaoying <shaoying@zuoyebang.com>
2020-09-08 19:53:45 +0800

    opt some

--- plugin/table.js
+++ plugin/table.js
@@ -23,7 +23,27 @@ Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, msg, 
         ])
         can.onappend.table(can, can.ui.content, "table", msg)
         can.onappend.board(can, can.ui.display, "board", msg)
+
+        can.page.Select(can, target, ".story", function(item) { var data = item.dataset
+            can.page.Modify(can, item, {style: can.base.Obj(data.style)})
+            var cb = can.onimport[data.type]; typeof cb == "function" && cb(can, data, item)
+        })
         return typeof cb == "function" && cb(msg)
+    },
+    spark: function(can, list, target) {
+        if (list["name"] == "inner") {
+            target.title = "点击复制"
+            target.onclick = function(event) {
+                can.user.copy(can, target.innerText)
+            }
+            return
+        }
+        can.page.Select(can, target, "span", function(item) {
+            item.title = "点击复制"
+            item.onclick = function(event) {
+                can.user.copy(can, item.innerText)
+            }
+        })
     },
 })
 Volcanos("onaction", {help: "控件交互", list: [],
