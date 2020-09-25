57894d2ed3939ec77e87cbe8eb73a9452aa73417
shaoying <shaoying@zuoyebang.com>
2020-09-25 10:55:16 +0800

    opt some

---
+++ plugin/input/paste.js
@@ -0,0 +1,3 @@
+Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, conf, list, cb, target) {
+    },
+})
--- plugin/local/wiki/word.js
+++ plugin/local/wiki/word.js
@@ -51,6 +51,7 @@ Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, msg, 
             sub.run = function(event, cmds, cb, silent) {
                 can.run(event, (cmds[0] == "search"? []: ["action", "story", item.type, item.name, item.text]).concat(cmds), cb, true)
             }
+            delete(target.dataset.meta)
         }, can._output, target)
     },
 
