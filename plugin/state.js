db3dcdb474934618604c765553398665efa28673
shaoying <shylinux@icloud.com>
2021-08-06 09:13:13 +0800

    opt some

--- plugin/state.js
+++ plugin/state.js
@@ -105,6 +105,7 @@ Volcanos("onaction", {help: "交互操作", list: [
     },
     "生成链接": function(event, can) { var meta = can.Conf()
         var args = can.Option(); args._path = "/chat/cmd/"+(meta.index||can.core.Keys(meta.ctx, meta.cmd))
+        args._path == "/chat/cmd/web.wiki.word" && (args = {_path: "/chat/cmd/"+args.path})
         var msg = can.request(event, {link: can.user.MergeURL(can, args)})
         can.search(event, ["Header.onaction.share"])
     },
