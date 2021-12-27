6c297a9192424b05481fec76ebe1289eb38e77b9
harveyshao <harveyshao@tencent.com>
2021-12-27 23:03:11 +0800

    opt some

--- panel/cmd.js
+++ panel/cmd.js
@@ -1,24 +1,19 @@
 Volcanos("onaction", {help: "交互操作", list: [], _init: function(can, msg, list, cb, target) {
-        can.onmotion.float.auto(can, can._output, chat.CARTE)
-        can.onmotion.float.auto(can, window, chat.CARTE)
+        can.page.ClassList.add(can, can._target, "Action")
         can.base.isFunc(cb) && cb()
     },
     onlogin: function(can) { can._names = location.pathname
-        var msg = can.request({})
         can.Conf(chat.TOOL)? can.core.Next(can.Conf(chat.TOOL), function(item, next) {
-            can.core.Timer(500, function() {
-                can.onaction._plugin(can, item, next)
-            })
+            can.core.Timer(500, function() { can.onaction._plugin(can, item, next) })
 
-        }): can.run(msg._event, [ctx.ACTION, ctx.COMMAND], function(msg) {
+        }): can.run(can.request()._event, [ctx.ACTION, ctx.COMMAND], function(msg) {
             can.core.Next(msg.Table(), function(item, next) {
                 can.onaction._plugin(can, item, next)
             })
         })
-        can.page.ClassList.add(can, can._target, "Action")
     },
     _plugin: function(can, item, next) {
-        item.height = window.innerHeight, item.width = window.innerWidth, item.opts = can.misc.Search(can)
+        can.base.Copy(item, {height: window.innerHeight, width: window.innerWidth, opts: can.misc.Search(can)})
         can.onappend.plugin(can, item, function(sub, meta) { can.user.title(meta.name), next() })
     },
 })
