31afa597b59fb56f8cfbc3c0f1de09cfdd099eaa
harveyshao <harveyshao@tencent.com>
2022-04-01 09:27:47 +0800

    opt some

---
+++ plugin/story/form.js
@@ -0,0 +1,62 @@
+Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, msg, cb, target) {
+		can.page.ClassList.add(can, target, "form")
+		msg.Push("type", "text")
+		msg.Push("name", "name")
+		msg.Push("value", "harveyshao")
+		msg.Push("action", "key")
+
+		msg.Push("type", "text")
+		msg.Push("name", "age")
+		msg.Push("value", "18")
+		msg.Push("action", "")
+
+		msg.Push("type", "select")
+		msg.Push("name", "male")
+		msg.Push("value", "male,female")
+		msg.Push("action", "")
+
+		msg.Push("type", "textarea")
+		msg.Push("name", "intro")
+		msg.Push("value", "program")
+		msg.Push("action", "")
+
+		msg.Push("type", "button")
+		msg.Push("name", "submit")
+		msg.Push("value", "提交")
+		msg.Push("action", "")
+
+		var action = ""; msg.Table(function(value, index, array) {
+			switch (value.type) {
+				case html.BUTTON: action = action||value.name
+					can.page.Append(can, target, [{view: "item", list: [
+						{view: ["input"], list: [{type: html.INPUT, style: {width: (can.ConfWidth()-90)/2}, data: {type: value.type, value: value.value}, onclick: function(event) {
+							var args = [ctx.ACTION, action]
+							can.page.Select(can, target, ".args", function(item) { args.push(item.name, item.value)})
+							can.run(event, args)
+						}}]},
+					]}])
+					break
+				case html.SELECT:
+					can.page.Append(can, target, [{view: "item", list: [
+						{view: ["label"], list: [{text: value.name}]},
+						{view: ["input"], list: [{type: html.SELECT, className: "args", style: {width: can.ConfWidth()-100},
+							data: value, list: can.core.List(can.core.Split(value.value), function(v) { return {type: html.OPTION, name: v, value: v, inner: v} }) }]},
+					]}])
+					break
+				case html.TEXTAREA:
+					can.page.Append(can, target, [{view: "item", list: [
+						{view: ["label"], list: [{text: value.name}]},
+						{view: ["input"], list: [{type: html.TEXTAREA, className: "args", style: {width: can.ConfWidth()-100}, data: value}]},
+					]}])
+					break
+				default:
+					can.page.Append(can, target, [{view: "item", list: [
+						{view: ["label"], list: [{text: value.name}]},
+						{view: ["input"], list: [{type: "input", className: "args", style: {width: can.ConfWidth()-100}, data: value}]},
+					]}])
+			}
+		})
+	},
+}, [""])
+
+
--- plugin/story/trend.js
+++ plugin/story/trend.js
@@ -5,7 +5,7 @@ Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, msg, 
 		can.Conf(html.HEIGHT, can.Conf(html.HEIGHT)||200)
 
 		can.msg = msg, can.data = msg.Table(), can.onimport._sum(can)
-		can.Action(html.HEIGHT, msg.Option(html.HEIGHT)||"max")
+		can.Action(html.HEIGHT, msg.Option(html.HEIGHT)||"auto")
 		can.Action("speed", parseInt(msg.Option("speed")||"100"))
 
 		can.require(["/plugin/local/wiki/draw.js", "/plugin/local/wiki/draw/path.js"], function() {
@@ -43,13 +43,13 @@ Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, msg, 
 		can.onaction[can.Action("view")]({}, can)
 	},
 }, [""])
-Volcanos("onaction", {help: "组件菜单", list: ["edit", ["view", "趋势图", "柱状图", "数据源"], ["height", "100", "200", "400", "600", "800", "max"], ["speed", "10", "20", "50", "100"]],
+Volcanos("onaction", {help: "组件菜单", list: ["edit", ["view", "趋势图", "柱状图", "数据源"], ["height", "100", "200", "400", "600", "800", "auto"], ["speed", "10", "20", "50", "100"]],
 	"edit": function(event, can) {
 		can.onmotion.toggle(can, can._action)
 		can.onmotion.toggle(can, can._status)
 	},
 	"趋势图": function(event, can) { var height = can.Action(html.HEIGHT)
-		if (height == "max") {
+		if (height == "auto") {
 			height = can.Conf(html.HEIGHT)
 		}
 		height = parseInt(height)
