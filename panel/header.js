0590f1cf08fc18e1f543985e7f01bb1c869f69eb
harveyshao <harveyshao@tencent.com>
2022-04-22 19:11:54 +0800

    opt some

--- panel/header.js
+++ panel/header.js
@@ -87,7 +87,7 @@ Volcanos("onimport", {help: "导入数据", list: [], _init: function(can, msg, 
 		msg.Option(aaa.AVATAR) && can.page.Modify(can, "div.state.avatar>img", {src: "/share/local/avatar"})
 	},
 	_menus: function(can, msg, target) {
-		can.setHeaderMenu(can.user.mod.isPod||can.user.isExtension||can.user.isMobile? []:
+		can.setHeaderMenu(can.user.mod.isPod||can.user.isExtension||can.user.isMobile? [["发布", "local", "invite"]]:
 			can.base.Obj(msg.Option(chat.MENUS)||can.Conf(chat.MENUS), can.onaction._menus), function(event, button) {
 				can.core.CallFunc(can.onaction[button]||function(event, can) {
 					can.run(event, [button], function(msg) { can.user.toast(can, "执行成功", can.user.trans(can, button)) })
@@ -186,6 +186,17 @@ Volcanos("onaction", {help: "交互数据", list: [], _init: function(can, cb, t
 	white: function(event, can, button) { can.onlayout.topic(can, button), can.onlayout._init(can) },
 	print: function(event, can, button) { can.onlayout.topic(can, can.base.join([chat.WHITE, button]))
 		can.set("River", html.HEIGHT, -1), can.set("Action", html.HEIGHT, -1)
+	},
+	local: function(event, can) {
+		can.run(event, ["local"], function(msg) { can.user.toast(can, "配置成功") })
+	},
+	"invite": function(event, can, button, river, storm) {
+		can.run(event, [ctx.ACTION, aaa.INVITE], function(msg) {
+			can.user.toast(can, {
+				title: "下载应用", duration: -1, width: -300,
+				content: msg.Result(), action: [cli.CLOSE],
+			})
+		})
 	},
 	webpack: function(event, can) {
 		can.user.input(event, can, [{name: mdb.NAME, value: can.user.title()}], function(ev, button, meta, list) {
