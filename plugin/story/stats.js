d5830c34cd76a7e8c56c1d75878a5575c7cb7f2a
shy <shy@shylinux.com>
2023-11-16 16:49:02 +0800

    add stats

--- plugin/story/stats.js
+++ plugin/story/stats.js
@@ -1,17 +1,18 @@
 Volcanos(chat.ONIMPORT, {
 	_init: function(can, msg) { can.onappend.style(can, "stats", can._output)
-		// can.onimport.layout = function() { can.onlayout.expand(can, can._output, 150, 100) }
-		var units = {}
-		var stats = {}; msg.Table(function(value) {
+		var units = {}, stats = {}; msg.Table(function(value) { units[value.name] = value.units
 			stats[value.name] = parseFloat(stats[value.name]||"0") + parseFloat(value.value)
-			units[value.name] = value.units
 		})
-		can.core.Item(stats, function(name, value) {
-			can.page.Append(can, can._output, [{view: [[html.ITEM, name]], list: [
-				{view: [mdb.VALUE, "", can.base.trimSuffix(parseFloat(value).toFixed(2)+"", ".00")+" "+units[name]]},
-				{view: [mdb.NAME, "", can.user.trans(can, name, null, html.INPUT)]},
-			]}])
-		})
-		can.isCmdMode() && can.onappend.table(can, msg)
+		function fmts(value) { var ls = []
+			while (value > 0) { ls.push(value%1000)
+				if (ls.length == 1) { ls[0] = ls[0].toFixed(2) }
+				value = parseInt(value/1000)
+			}
+			return ls.reverse().join(", ")
+		}
+		can.core.Item(stats, function(name, value) { can.page.Append(can, can._output, [{view: [[html.ITEM, name]], list: [
+			{view: [mdb.VALUE, "", can.base.trimSuffix(fmts(parseFloat(value).toFixed(2))+"", ".00")+" "+units[name]]},
+			{view: [mdb.NAME, "", can.user.trans(can, name, null, html.INPUT)]},
+		]}]) }), can.isCmdMode() && can.onappend.table(can, msg)
 	},
 })
