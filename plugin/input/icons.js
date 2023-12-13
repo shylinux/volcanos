b090c170202ab2e0dab6009c8a6b79a31759e23b
shy <shy@shylinux.com>
2023-12-13 19:10:18 +0800

    opt icons

--- plugin/input/icons.js
+++ plugin/input/icons.js
@@ -1,13 +1,13 @@
 Volcanos(chat.ONFIGURE, {icons: {
 	_load: function(event, can, cb, target, name, value) {
-		can.runAction(event, ctx.RUN, ["nfs.dir", "usr/icons/"], function(msg) { can._show(can, msg, cb, target, name) })
+		can.runAction(event, mdb.INPUTS, [mdb.ICONS], function(msg) { can._show(can, msg, cb, target, name) })
 	},
 	_show: function(can, msg, cb, target, name) { can.onmotion.clear(can)
 		var table = can.page.Append(can, can._output, [{type: html.TABLE}])._target, tr
 		msg.Table(function(value, index) { if (index%5 == 0) { tr = can.page.Append(can, table, [{type: html.TR}])._target }
-			can.page.Append(can, tr, [{type: html.TD, title: value.path, list: [{img: can.misc.Resource(can, value.path), style: {width: 60, height: 60}}], onclick: function() {
-				can.close(), can.base.isFunc(cb) && cb(can, value.path, target.value)
-				target._icon.src = can.misc.Resource(can, value.path)
+			can.page.Append(can, tr, [{type: html.TD, title: value.icons, list: [{img: can.misc.Resource(can, value.icons), style: {width: 60, height: 60}}], onclick: function() {
+				can.close(), can.base.isFunc(cb) && cb(can, value.icons, target.value)
+				target._icon.src = can.misc.Resource(can, value.icons)
 			}}])
 		}), can.onappend._status(can, [mdb.TOTAL]), can.Status(mdb.TOTAL, msg.Length())
 		can.onlayout.figure({target: target}, can, can._target, false, 200)
