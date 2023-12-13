7ccc8dce087afe469e2aedabbf47cf895a801a52
shy <shy@shylinux.com>
2023-12-13 09:49:09 +0800

    add some

--- plugin/input.js
+++ plugin/input.js
@@ -1,7 +1,6 @@
 Volcanos(chat.ONACTION, {
 	run: function(event, can) { can.run(can.request(event, {_method: http.POST})) }, refresh: function(event, can) { can.run(can.request(event, {_method: http.GET})) },
-	list: function(event, can) {
-		can.sup.isSimpleMode() || can.run(can.request(event, {_toast: event.isTrusted? ice.PROCESS: "" , _method: http.GET})) }, back: function(event, can) { can.sup.onimport.back(event, can.sup) },
+	list: function(event, can) { can.sup.isSimpleMode() || can.run(can.request(event, {_toast: event.isTrusted? ice.PROCESS: "" , _method: http.GET})) }, back: function(event, can) { can.sup.onimport.back(event, can.sup) },
 	onclick: function(event, can) { can.Conf(mdb.TYPE) == html.BUTTON && can.run(event, [ctx.ACTION, can.Conf(mdb.NAME)].concat(can.sup.Input())), can.onkeymap.prevent(event) },
 	onchange: function(event, can) { can.Conf(mdb.TYPE) == html.SELECT && can.run(event) },
 	onkeydown: function(event, can) { can.onkeymap.input(event, can, event.target); if (can.Conf(mdb.TYPE) == html.TEXTAREA && !event.ctrlKey) { return }
