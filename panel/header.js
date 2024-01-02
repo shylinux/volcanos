20ad6179b69174b52ed0db0d996aeadebfc599eb
shy <shy@shylinux.com>
2024-01-02 16:15:38 +0800

    opt aaa.sess

--- panel/header.js
+++ panel/header.js
@@ -63,7 +63,8 @@ Volcanos(chat.ONACTION, {_init: function(can) {},
 	onsize: function(can) { can.ConfHeight(can._target.offsetHeight), can.ConfWidth(can._target.offsetWidth) },
 	onmain: function(can) {
 		if (!can.user.isMailMaster) { if (can.misc.Search(can, ice.MSG_SESSID)) { can.misc.CookieSessid(can, can.misc.Search(can, ice.MSG_SESSID)); return can.misc.Search(can, ice.MSG_SESSID, "") } }
-		function lang(msg, cb) { can.user.info.language = msg.SearchOrOption(aaa.LANGUAGE)
+		function lang(msg, cb) {
+			can.user.info.language = msg.SearchOrOption(aaa.LANGUAGE)
 			can.user.info.language? can.require([can.misc.Resource(can, nfs.SRC_TEMPLATE+web.CHAT_HEADER+"/language/"+can.user.info.language+".js")], cb, function(can, name, sub) { can.base.Copy(can.user._trans, sub._trans) }): cb && cb()
 			can.onmotion.delay(can, function() { can.onimport._language(can) })
 		}
