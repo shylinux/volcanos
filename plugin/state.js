d221885a7d0dd924bf5f1d5956d69bb3d5ae5161
shy <shy@shylinux.com>
2024-04-19 17:58:07 +0800

    add some

--- plugin/state.js
+++ plugin/state.js
@@ -109,7 +109,7 @@ Volcanos(chat.ONIMPORT, {
 	})[0] },
 	_size: function(can, height, width, auto, mode) {},
 	size: function(can, height, width, auto, mode) { typeof width == code.STRING && (width = can.base.ParseSize(width))
-		can.Conf("_auto", auto), can.Mode(mode), can.ConfHeight(height), can.ConfWidth(width), height -= can.onexport.actionHeight(can)+(can.onexport.statusHeight(can)||1)
+		can.Conf("_auto", auto), can.Mode(mode), can.ConfHeight(height), can.ConfWidth(width), height -= can.onexport.actionHeight(can)+(can.onexport.statusHeight(can)||(can.sub? 0: 1))
 		var padding = can.Conf("padding")||0, margin = can.Conf("margin")||0; height -= 2*padding, width -= 2*padding+2*margin
 		auto || auto == undefined? (can.page.style(can, can._output, html.HEIGHT, "", html.WIDTH, "", html.MAX_HEIGHT, height, html.MAX_WIDTH, width), can.page.style(can, can._target, html.HEIGHT, "", html.WIDTH, "")):
 			(can.page.style(can, can._output, html.HEIGHT, height, html.WIDTH, width, html.MAX_HEIGHT, "", html.MAX_WIDTH, ""), can.page.style(can, can._target, html.WIDTH, width))
