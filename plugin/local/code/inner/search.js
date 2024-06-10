e99a96006f60685ca6e388050f3046632b3a3958
shy <shy@shylinux.com>
2024-06-10 09:31:57 +0800

    add some

--- plugin/local/code/inner/search.js
+++ plugin/local/code/inner/search.js
@@ -1,5 +1,5 @@
 Volcanos(chat.ONIMPORT, {list: [mdb.VALUE, "close:button"],
-	_init: function(can, msg) { msg.Defer(function() { can.onappend.scroll(can) })
+	_init: function(can, msg) { msg.Defer(function() { can.onappend.scroll(can) }), can.page.style(can, can._output, html.VISIBILITY, "")
 		can.onappend.table(can, msg), can.onappend.board(can, msg), can.onmotion.highlight(can, can.Option(mdb.VALUE, msg.Option(mdb.VALUE)))
 		can.page.Select(can, can._option, "input[name=value]", function(target) { can.onmotion.hidden(can, target) })
 	},
