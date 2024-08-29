7430e2b312ef38a13aae7d0fad4f47a1c9e50128
shy <shy@shylinux.com>
2024-08-29 11:09:30 +0800

    add some

--- panel/footer.js
+++ panel/footer.js
@@ -65,7 +65,14 @@ Volcanos(chat.ONACTION, {_init: function(can) {},
 	onsize: function(can) { can.ConfHeight(can._target.offsetHeight), can.ConfWidth(can._target.offsetWidth)
 		can.onimport.value(can, html.HEIGHT, can.page.height()), can.onimport.value(can, html.WIDTH, can.page.width())
 	},
-	onlogin: function(can, msg) { can.run(can.request({}, {_method: http.GET}), [], function(msg) { can.onmotion.clear(can), can.onimport._init(can, msg, can._output) }) },
+	onmain: function(can) {
+		can.run(can.request({}, {_method: http.GET}), [], function(msg) {
+			can.page.Appends(can, can._output, [{view: "item title", list: [{text: msg.Result()}]}])
+		})
+	},
+	onlogin: function(can, msg) {
+		can.run(can.request({}, {_method: http.GET}), [], function(msg) { can.onmotion.clear(can), can.onimport._init(can, msg, can._output) })
+	},
 	ontoast: function(can, msg) { can.core.CallFunc(can.onimport.ntip, {can: can, msg: msg}) },
 	onremote: function(can, msg) { can.core.CallFunc(can.onimport.ncmd, {can: can, msg: msg}) },
 	onunload: function(can) { can._wss && can._wss.close() },
