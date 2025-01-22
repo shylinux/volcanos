9f6650df0ddd2af71fa4587ed2daa88604854b51
shy <shy@shylinux.com>
2025-01-22 16:12:27 +0800

    add some

--- plugin/story/form.js
+++ plugin/story/form.js
@@ -5,7 +5,11 @@ Volcanos(chat.ONIMPORT, {
 			return {view: [[html.ITEM, item.type]], list: [item.type != html.BUTTON && {text: [can.user.trans(can, item.name, item._trans, html.INPUT), "", mdb.NAME]}, item.need == "must" && {text: ["*", "", "need"]}], _init: function(target) {
 				item.type == html.BUTTON && (item.onclick = function(event) { var args = []
 					can.core.Item(can.page.SelectArgs(can, can._output)[0], function(key, value) { args.push(key, value) })
-					can.Update(can.request(event, {_handle: ice.TRUE}), [ctx.ACTION, item.name].concat(args), function() {
+					can.Update(can.request(event, {_handle: ice.TRUE}), [ctx.ACTION, item.name].concat(args), function(msg) {
+						if (msg.IsErr()) {
+							can.user.toastFailure(can, msg.Result())
+							return
+						}
 						can.Update()
 					})
 				}), can.onappend.input(can, item, "", target)
